da2bfdaff2010180826a6214319d422446e48c7b
Udeme01 <emmanueluddy2@gmail.com>
2024-10-16 13:00:38 +0100

    created available places state and used the browser fetch api to get available places from my backend server

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,25 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePLaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+        console.log(resData);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePLaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
